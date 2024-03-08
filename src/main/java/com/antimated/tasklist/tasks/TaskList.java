e22dda8aeaa77a26127299c61af574f4a5cf32a2
Nicholas Olivier <antimated@gmail.com>
2024-03-08 22:11:52 +0100

    Expand methods in TaskList class

--- src/main/java/com/antimated/tasklist/tasks/TaskList.java
+++ src/main/java/com/antimated/tasklist/tasks/TaskList.java
@@ -1,45 +1,67 @@
 package com.antimated.tasklist.tasks;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
 
+@Slf4j
 @Getter
-public class TaskList {
+public class TaskList
+{
 	private final List<Task> tasks;
 
-	public TaskList(List<Task> tasks) {
+	public TaskList(List<Task> tasks)
+	{
 		this.tasks = tasks;
 	}
 
-	public Task getTaskById(int id) {
-		return tasks.stream()
-			.filter(task -> task.getId() == id)
-			.findFirst()
-			.orElse(null);
+	public TaskList getTasksByType(TaskType... types)
+	{
+		List<TaskType> typeList = Arrays.asList(types);
+
+		List<Task> filteredTasks = tasks.stream()
+			.filter(task ->  typeList.contains(task.getType()))
+			.collect(Collectors.toList());
+
+		return new TaskList(filteredTasks);
 	}
 
-	public List<Task> getTasksByType(TaskType type) {
-		return tasks.stream()
-			.filter(task -> task.getType() == type)
+	public TaskList getTasksByTiers(TaskTier... tiers)
+	{
+		List<TaskTier> tierList = Arrays.asList(tiers);
+
+		List<Task> filteredTasks = tasks.stream()
+			.filter(task ->  tierList.contains(task.getTier()))
 			.collect(Collectors.toList());
+
+		return new TaskList(filteredTasks);
 	}
 
-	public List<Task> getTasksByTier(TaskTier tier) {
-		return tasks.stream()
-			.filter(task -> task.getTier() == tier)
+	public TaskList getSatisfyingTasks(Client client)
+	{
+		List<Task> filteredTasks = tasks.stream()
+			.filter(task -> task.getRequirement().satisfiesRequirement(client))
 			.collect(Collectors.toList());
+
+		return new TaskList(filteredTasks);
 	}
 
-	public List<Task> getIncompleteTasks() {
-		return tasks.stream()
-			.filter(task -> !task.isCompleted())
+
+	public TaskList getTasksByCompletion(boolean isCompleted)
+	{
+		List<Task> filteredTasks = tasks.stream()
+			.filter(task -> task.isCompleted() == isCompleted)
 			.collect(Collectors.toList());
+
+		return new TaskList(filteredTasks);
 	}
 
-	public List<Task> getCompleteTasks() {
-		return tasks.stream()
-			.filter(Task::isCompleted)
-			.collect(Collectors.toList());
+	public List<Task> all()
+	{
+		log.debug("Tasks in all() query: {}", this.tasks);
+		return this.tasks;
 	}
 }
